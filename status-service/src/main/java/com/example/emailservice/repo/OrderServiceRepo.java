8c8a175574c4cbc2ab78cfba6999c23ae5b3296e
MukundPisal <MukundPisal@DESKTOP-VI2LROA>
2023-09-25 12:57:33 +0530

    push to gitHub

--- status-service/src/main/java/com/example/emailservice/repo/OrderServiceRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.emailservice.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.emailservice.model.OrderStatus;
-
-public interface OrderServiceRepo extends JpaRepository<OrderStatus, String>{}
--- stock-service/src/main/java/com/example/stockservice/repo/StockRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.stockservice.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.stockservice.model.Stock;
-
-public interface StockRepo extends JpaRepository<Stock, Integer>{
-
-}
